c61d86727143986faa683cf1c71e726edc250e61
Munteanu <munteanuvictoria1@gmail.com>
2022-12-28 15:53:33 +0100

    added synonym

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,23 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 
 export default function Meaning(props){
     return (
-        <div className="meaning">  
+        <div className="meaning"> 
+        <h3> {props.meaning.partOfSpeech}</h3> 
         {props.meaning.definitions.map(function(definition, index) 
         {
             return (
                 <div key= {index}> 
                 <p> 
+                
                 {definition.definition}
                 <br />
-                <em>{definition.example} </em>
+                Example:
+                <em> {definition.example} </em>
+               <Synonyms synonyms={definition.synonyms}/>
+            
                 </p>
                 </div>
             );
---
+++ src/Synonyms.js
@@ -0,0 +1,18 @@
+import React  from "react";
+
+
+
+export default function Synonyms(props){
+    if (props.synonyms) {
+    return (
+        <div className="synonyms"> 
+        {props.synonyms.map(function(synonym, index){
+            return <li key= {index}>{synonym}</li>; 
+            })}
+         </div>
+        );
+
+} else {
+    return null;
+}
+}
