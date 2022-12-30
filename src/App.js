9fc027abf6906335a50127fe0c2c31ddf2b9276e
Munteanu <munteanuvictoria1@gmail.com>
2022-12-30 12:01:26 +0100

    style update

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div className="App">
       <header className="container">
         <h1>Dictionary</h1>
-        <h5> <em> "Words, are of course the most poweful drug used by manking" <a href="https://www.britannica.com/biography/Rudyard-Kipling" target="_blank" rel="noreferrer">Rudyard Kipling</a></em></h5>
+      
         <Dictionary />
        </header>
-      <footer className="container">
-        Project coded by <a href= " http://victoriamunteanuportfolio.net/" target="_blank" rel="noreferrer" > Victoria Munteanu</a>
+      <footer className="footer">
+        Project coded by <a href= " http://victoriamunteanuportfolio.net/" target="_blank" rel="noreferrer" >  Victoria Munteanu</a>
       </footer>
     </div>
   );
