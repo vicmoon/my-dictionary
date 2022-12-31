182a5940400a5ce86d5d9a71ebe9d2d8b8731cdd
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2022-12-31 18:40:42 +0100

    update

--- src/Photo.js
+++ src/Photo.js
@@ -14,7 +14,7 @@ export default function Photos(props) {
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   {" "}
-                  <img src={photo.src.landscape} className="img-fluid" />
+                  <img src={photo.src.landscape} className="img-fluid" alt="" />
                 </a>
               </div>
             );
